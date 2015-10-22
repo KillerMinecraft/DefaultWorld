fdd30d593948b4e8d7239902b1be27514b2c982e
FTWinston <ftwinston@gmail.com>
2015-10-23 00:01:54 +0100

    Implemented getWorldType

--- src/com/ftwinston/KillerMinecraft/Modules/DefaultWorld/Plugin.java
+++ src/com/ftwinston/KillerMinecraft/Modules/DefaultWorld/Plugin.java
@@ -1,12 +1,16 @@
 package com.ftwinston.KillerMinecraft.Modules.DefaultWorld;
 
 import org.bukkit.Material;
+import org.bukkit.World.Environment;
 
 import com.ftwinston.KillerMinecraft.WorldGenerator;
 import com.ftwinston.KillerMinecraft.WorldGeneratorPlugin;
 
 public class Plugin extends WorldGeneratorPlugin
 {
+	@Override
+	public Environment getWorldType() { return Environment.NORMAL; }
+	
 	@Override
 	public String[] getDescriptionText() { return new String[] {"Using the default world generator"}; }
 	
